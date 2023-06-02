085119fddb2a6570e9eac5224069e45a5391625a
ramcharan7251 <128666597+ramcharan7251@users.noreply.github.com>
2023-06-02 17:32:44 +0530

    functions=without parameters ,with return type

---
+++ without parameters and with return type.c
@@ -0,0 +1,15 @@
+#include<stdio.h>              
+int sum()
+{
+     int n1,n2;
+     scanf("%d%d",&n1,&n2);
+     return n1+n2;
+}
+int main()
+{
+   	 int sum();              
+	printf("%d",sum());
+	return 0;
+}
+
+
